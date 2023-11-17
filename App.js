df1fd203518ccb2146f9d0c585fbd9ad487ad91a
adrianooandrade <allonan.619@gmail.com>
2023-11-17 00:42:51 +0000

    Added loop rendering; reduced css;

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { useState } from "react";
 
 export default function App() {
@@ -9,43 +17,36 @@ export default function App() {
     setEnteredGoalText(enteredText);
   };
 
-  const addGoalHandler = () => {
+  function addGoalHandler() {
     // setCourseGoals([...courseGoals, enteredGoalText]);
     // or (recommended when the new data uses the previews data)
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       enteredGoalText,
     ]);
-  };
+  }
 
   return (
     <View style={styles.appContainer}>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          gap: 8,
-          alignItems: "center",
-        }}
-      >
+      <View style={styles.actions}>
         <TextInput
           placeholder={"What are your goals in life?"}
-          style={{
-            flex: 1,
-            borderWidth: 2,
-            borderColor: "#cccccc",
-            minHeight: 38,
-            margin: 4,
-          }}
-          onTextInput={goalInputHandler}
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
         ></TextInput>
         <Button title={"Add goal"} onPress={addGoalHandler}></Button>
       </View>
-      <View style={{ flex: 10 }}>
-        <Text>List of goals</Text>
+      <View style={{ flex: 10, gap: 8 }}>
+        <ScrollView>
+          <View style={{ flex: 1, gap: 8 }}>
+            {courseGoals.map((goal) => (
+              <View key={goal} style={styles.goalItem}>
+                <Text style={{ color: "white" }}>{goal}</Text>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
       </View>
-      <View></View>
     </View>
   );
 }
@@ -54,5 +55,25 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     margin: 40,
+    gap: 10,
+  },
+  actions: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 8,
+    alignItems: "center",
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: "#cccccc",
+    minHeight: 38,
+    margin: 4,
+  },
+  goalItem: {
+    borderRadius: 6,
+    backgroundColor: "#2296f3",
+    alignItems: "center",
   },
 });
