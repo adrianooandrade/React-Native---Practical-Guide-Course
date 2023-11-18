71a863195550195232e464150f62c43379aaf57b
adrianooandrade <allonan.619@gmail.com>
2023-11-18 00:37:47 +0000

    Added ripple for android and fade for IOS

--- components/GoalItem/GoalItem.js
+++ components/GoalItem/GoalItem.js
@@ -2,8 +2,14 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function GoalItem(props) {
   return (
-    <View>
-      <Pressable onPress={props.onPressItem.bind(this, props.id)}>
+    <View style={{ borderRadius: 6, backgroundColor: "#2296f3" }}>
+      <Pressable
+        android_ripple={{ color: "0064F3" }}
+        style={(pressedData) => {
+          return pressedData.pressed ? styles.pressedItem : null;
+        }}
+        onPress={props.onPressItem.bind(this, props.id)}
+      >
         <View style={styles.goalItem}>
           <Text style={{ color: "white" }}>{props.text}</Text>
         </View>
@@ -16,10 +22,11 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
-    borderRadius: 6,
-    backgroundColor: "#2296f3",
     alignItems: "center",
     minHeight: 32,
     justifyContent: "center",
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
