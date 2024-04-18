592ea2f23f14d31eede27024146dacfc381f0d93
adrianooandrade <allonan.619@gmail.com>
2024-04-18 18:22:28 +0100

    Update App.js

--- Meals-App/App.js
+++ Meals-App/App.js
@@ -4,6 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -11,14 +12,43 @@ export default function App() {
     <>
       <StatusBar></StatusBar>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#BE9163" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#eabd8e" },
+          }}
+        >
           <Stack.Screen
             name={"Meals Categories"}
             component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
           ></Stack.Screen>
           <Stack.Screen
             name={"Meals Overview"}
             component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return {
+            //     title: catId,
+            //   };
+            // }}
+          ></Stack.Screen>
+
+          <Stack.Screen
+            name={"Meal Details"}
+            component={MealDetailsScreen}
+            // options={{
+            //   headerRight: () => {
+            //     return (
+            //       <View>
+            //         <Button title="Favourite"> In the header</Button>
+            //       </View>
+            //     );
+            //   },
+            // }}
           ></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
